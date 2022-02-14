42c0a89da04ff74bcbadc1b09bfa3da5c8c56e83
张凤 <zhangfeng@cloudcc.com>
2022-02-14 20:59:51 +0800

    提交代码

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+const exec = require('child_process').exec
+
+fs.writeFileSync('./test/hello.txt', 'world')
+
+exec('git init && git add . && git commit -m "提交" && git pull && git push', { cwd: './test' }, (err, stdout, stderr) => {
+  // ...
+  console.log(stdout)
+})
