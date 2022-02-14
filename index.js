350012fc07b73d02b738f7a298d7e7284b1b4f3c
张凤 <zhangfeng@cloudcc.com>
2022-02-14 21:20:13 +0800

    提交代码

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const exec = require('child_process').exec
 
-fs.writeFileSync('./test/hello.txt', '55555555555555555555')
+fs.writeFileSync('./test/hello.txt', '666666666666')
 
 exec(' git add . && git commit -m "提交代码" && git pull && git push', { cwd: './' }, (err, stdout, stderr) => {
   // ...
