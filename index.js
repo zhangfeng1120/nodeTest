ac2fabf3dbf387a280343963871b3c7817601c68
张凤 <zhangfeng@cloudcc.com>
2022-02-14 21:08:48 +0800

    提交代码

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ fs.writeFileSync('./test/hello.txt', 'world')
 
 exec('git init && git add . && git commit -m "提交" && git pull && git push', { cwd: './test' }, (err, stdout, stderr) => {
   // ...
-  console.log(stdout)
+//   console.log(stdout)
 })
